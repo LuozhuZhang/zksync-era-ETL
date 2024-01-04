eb3974ae35b1530266634790d49ff14fa9545670
Luozhu <hedgefund996@gmail.com>
2024-01-04 17:11:15 +0000

    setup RPC & block range, etc.

---
+++ era/setup/tokens.py
@@ -0,0 +1,8 @@
+from web3 import Web3
+
+TOKEN_LIST = {
+  'WETH': Web3.to_checksum_address('0x5AEa5775959fBC2557Cc8789bC1bf90A239D9a91'),
+  'USDC': Web3.to_checksum_address('0x3355df6D4c9C3035724Fd0e3914dE96A5a83aaf4'),
+  'USDT': Web3.to_checksum_address('0x493257fd37edb34451f62edf8d2a0c418852ba4c'),
+  'BUSD': Web3.to_checksum_address('0x2039bb4116B4EFc145Ec4f0e2eA75012D6C0f181'),
+}
